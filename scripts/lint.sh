ec184838291ec3c34cf578323990da7a7464e8c6
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2024-04-07 15:12:08 +0200

    Add lint script

---
+++ scripts/lint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+SCRIPT_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd -P)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd -P)"
+cd "$PROJECT_ROOT" || exit 1
+
+PYTHON_BINARY='diskstats.py'
+BASH_BINARY='diskstats.sh'
+
+
+if ! command -v mypy > /dev/null 2> /dev/null; then
+    echo 'Error: Can not run mypy!' > /dev/stderr
+    echo 'mypy is not installed!' > /dev/stderr
+else
+    echo 'Running mypy...'
+    mypy $PYTHON_BINARY
+fi
+echo ''
+echo ''
+
+
+if ! command -v pylint > /dev/null 2> /dev/null; then
+    echo 'Error: Can not run pylint!' > /dev/stderr
+    echo 'pylint is not installed!' > /dev/stderr
+else
+    echo 'Running pylint...'
+    pylint $PYTHON_BINARY
+fi
+echo ''
+echo ''
+
+
+if ! command -v shellcheck > /dev/null 2> /dev/null; then
+    echo 'Error: Can not run shellcheck!' > /dev/stderr
+    echo 'shellcheck is not installed!' > /dev/stderr
+else
+    echo 'Running shellcheck...'
+    shellcheck $BASH_BINARY
+fi
+echo ''
+echo ''
